7a2dac732054b3da765fc99b57221f50ca1b390a
Ken Warren <134076996+ken-warren@users.noreply.github.com>
2024-05-21 14:16:15 +0300

    password strength checker

---
+++ password_strength.py
@@ -0,0 +1,29 @@
+# Let minimum requirements for the password be;
+# length between 8 and 16 characters
+# Minimum of 1 lowercase, uppercase, special character and number
+# no other characters allowed 
+
+w, x, y, z = 0, 0, 0, 0
+
+password = input('Enter Password: ')
+uppercase = 'ABXDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+special_char = '!@£$%^&*()_+'
+num = '0123456789'
+
+if (len(password) >= 8 and len(password) <= 16):
+    for i in password:
+        if (i in uppercase):
+            w += 1
+        if (i in lowercase):
+            x += 1
+        if (i in special_char):
+            y += 1
+        if (i in num):
+            z +- 1
+        
+if (w >= 1 and x >= 1 and y >= 1 and z >= 1
+                    and w + x + y + z == len(password)):
+    print('Password Valid')
+else:
+    print('Password Invalid. Please Try Again')
